adeeebbc74acf7c44b14581e6d20009b262c1fa8
tiagoaOliveira <tiagoaoliveira05@gmail.com>
2026-02-03 21:05:22 -0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import Login from './pages/Login';
 import Home from './pages/Home';
